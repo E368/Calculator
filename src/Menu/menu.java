01763c05c579e5d4d103c323eed77c8996994904
E368 <arnaud.piquet@wanadoo.fr>
2018-01-30 17:12:43 +0100

    menuterminé3

--- src/Menu/menu.java
+++ src/Menu/menu.java
@@ -4,7 +4,9 @@ public class menu {
 
 	public static void main(String[] args) {
 		System.out.println("Choisir le type d'opération");
-
+		System.out.println("Choisir un premier chiffre");
+		System.out.println("Choisir un deuxième chiffre");
+		System.out.println("Voici votre résultat !");
 	}
 
 }
